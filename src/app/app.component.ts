9932e457622db3209c31d08166fe4b1475233d83
IrinaSiandetskaya <irene_siv@inbox.ru>
2019-08-23 18:33:35 +0300

    start routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'shop';
+  title = "shop";
+
+  onActivate($event: any, routerOutlet: RouterOutlet) {
+    console.log("Activated Component", $event, routerOutlet);
+  }
+  onDeactivate($event: any, routerOutlet: RouterOutlet) {
+    console.log("Deactivated Component", $event, routerOutlet);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { Router } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { HttpClientModule } from '@angular/common/http';
-import { CartModule } from './cart/cart.module';
-import { ProductsModule } from './products/products.module';
-import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+
+import { CartModule } from "./cart/cart.module";
+import { ProductsModule } from "./products/products.module";
+import { SharedModule } from "./shared/shared.module";
+import { LayoutModule } from "./layout/layout.module";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     CartModule,
     ProductsModule,
-    SharedModule
+    SharedModule,
+    LayoutModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    const replacer = (key: string, value: any): string =>
+      typeof value === "function" ? value.name : value;
+    console.log("Routes: ", JSON.stringify(router.config, replacer, 2));
+  }
+}
